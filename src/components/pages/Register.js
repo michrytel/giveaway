013da68d653e6148053ebd43fdeb13b9d4b32f90
michrytel <76482065+michrytel@users.noreply.github.com>
2021-03-26 14:47:58 +0100

    register

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,11 +1,35 @@
 import Header from "../organisms/Header";
+import Title from "../atoms/Title";
+import {NavLink} from "react-router-dom";
 
 
 const Register = () => {
+    const handleSubmit = () => {}
     return (
         <>
             <Header/>
             <main>
+                <div className='login__container'>
+                    <Title text='Register' />
+                    <form className='form' onSubmit={handleSubmit}>
+                        <div className='form__inputs'>
+                            <label htmlFor='email'>Email</label>
+                            <input id='email' type='email' htmlFor='email' />
+                            <label htmlFor='password'>Password</label>
+                            <input id='password' type='password' htmlFor='password' />
+                            <label htmlFor='passwordRepeat'>Repeat password</label>
+                            <input id='passwordRepeat' type='password' />
+                        </div>
+                        <div className='form__btns'>
+                            <NavLink to='/login' activeClassName='active'>
+                                Login
+                            </NavLink>
+                            <button type='submit'>
+                                Register
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </main>
         </>
     );
